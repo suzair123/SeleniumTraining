96f3655d185d6ee3a6d98de53427352496aa6687
Syed Uzairuddin <syed.uzairuddin@10pearls.local>
2019-01-31 18:13:53 +0500

    Maintaing Selenium Training code to central repo

---
+++ firstCase/src/main/java/Config/ConfigTest.java
@@ -0,0 +1,32 @@
+package Config;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigTest {
+
+    Properties prop = new Properties();
+
+    public ConfigTest(String fileName) throws IOException {
+
+        try{
+
+            File fIn = new File(System.getProperty("user.dir")+"\\src\\main\\resources\\config\\"+ fileName+".properties");
+
+            if(fIn.exists()){
+                FileInputStream fInp = new FileInputStream(fIn);
+                prop.load(fInp);
+            }else {
+                System.out.println("File Not Found");
+            }
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    public String getProperty(String key){
+        String value = prop.getProperty(key);
+        return value;
+    }
+}
